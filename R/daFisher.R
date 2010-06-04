6020e31397811ed86273f825d7010a9f46f98a8a
Matthias Templ <templ@tuwien.ac.at>
2010-06-04 00:00:00 +0000

    version 1.4.0

--- R/daFisher.R
+++ R/daFisher.R
@@ -14,11 +14,11 @@ if(coda){
 	
 
 
-p <- ncol(x)
-ni <- table(grp)
-ng <- length(ni)
-n <- sum(ni)
-pi <- ni/n
+	p <- ncol(x)
+	ni <- table(grp)
+	ng <- length(ni)
+	n <- sum(ni)
+	pi <- ni/n
 if (method=="classical"){
   muil <- by(x,factor(grp),mean)
   sigil <- by(x,factor(grp),cov)
@@ -26,26 +26,75 @@ if (method=="classical"){
 else {
 #  require(rrcov)
   res <- by(x,factor(grp),CovMcd)
-  print(res)
   muil <- lapply(res,getCenter)
   sigil <- lapply(res,getCov)
 }
 
-mui <- matrix(unlist(muil),ng,p,byrow=TRUE)
-mu <- pi%*%mui
-hlp <- diag(sqrt(pi))%*%(mui-rep(1,ng)%*%mu)
-B <- t(hlp)%*%hlp
-sigi <- array(unlist(sigil),dim=c(p,p,ng))
-W <- apply(sigi*array(sort(rep(pi,p*p)),dim=c(p,p,ng)),c(1,2),sum)
-adir <- matrix(as.real(eigen(solve(W)%*%B)$vec),ncol=p)
-adirs <- t(t(adir)/(sqrt(diag(t(adir)%*%W%*%adir))))
+	mui <- matrix(unlist(muil),ng,p,byrow=TRUE)
+	mu <- pi%*%mui
+	hlp <- diag(sqrt(pi))%*%(mui-rep(1,ng)%*%mu)
+	B <- t(hlp)%*%hlp
+	sigi <- array(unlist(sigil),dim=c(p,p,ng))
+	W <- apply(sigi*array(sort(rep(pi,p*p)),dim=c(p,p,ng)),c(1,2),sum)
+	adir <- matrix(as.real(eigen(solve(W)%*%B)$vec),ncol=p)
+	adirs <- t(t(adir)/(sqrt(diag(t(adir)%*%W%*%adir))))
+	scores=x%*%adirs
 if(plotScore){
-  plot(x%*%adirs[,1:2],col=grp, pch=grp, cex=1.5)
+  plot(scores[,1:2],col=grp, pch=grp, cex=1.5)
 }
-res <- list(B=B,W=W,loadings=adir,coda=coda)
-class(res) <- "daFisher"
+#    postgroup <- apply(scores, 1, which.min)
+#	print(postgroup)
+	res <- list(B=B,W=W,loadings=adir,scores=scores,#classification=postgroup, 
+			coda=coda)
+	class(res) <- "daFisher"
+	
+#	## fill in for class lda
+#	g <- as.factor(grp)
+#	lev <- lev1 <- levels(g)
+#	counts <- as.vector(table(g))
+#	prior <- counts/n
+#	prior <- prior[counts > 0]
+#
+#if(method == "moment") fac <- 1/(n-ng) else fac <- 1/n
+#X <- sqrt(fac) * (x - group.means[g,  ]) %*% scaling
+#X.s <- svd(X, nu = 0)
+#X <-  sqrt(nu/(nu-2)*(1 + p/nu)/n * w) * (x - group.means[g,  ]) %*% scaling
+#X.s <- svd(X, nu = 0)
+#	cl <- match.call()
+#	cl[[1L]] <- as.name("daFisher")
+#	
+#	res <- structure(list(prior = prior, counts = counts, means = mui,
+#					scaling = hlp, lev = lev, svd = hlp,
+#					N = n, call = cl, B=B, W=W, loadings=adir, coda=coda),
+#			class = "lda")
+#
+#	z1=z[grp=="arabica",]
+#	z2=z[grp=="blended",]
+#	n1=nrow(z1)
+#	n2=nrow(z2)
+#	n=n1+n2
+#	p1=n1/n
+#	p2=n2/n
+#	m1=apply(z1,2,mean)
+#	m2=apply(z2,2,mean)
+#	S1=cov(z1)
+#	S2=cov(z2)
+#	Sp=((n1-1)/(n1-1+n2-1))*S1+((n2-1)/(n1-1+n2-1))*S2
+#	Sp1=solve(Sp)
+#	yLDA=as.numeric(t(m1-m2)%*%Sp1%*%t(z)-as.numeric(1/2*t(m1-m2)%*%Sp1%*%(m1+m2)))-log(p2/p1)	
+#	plot(z, pch=21, bg=ifelse(grp=="arabica","red","blue"))#bg=ifelse(yLDA<0,"red","blue"))
+#	y1=seq(from=min(z[,1])-1.5,to=max(z[,1])+1.9,by=0.05)
+#	y2=seq(from=min(z[,2]),to=max(z[,2])+0.2,by=0.05)
+#	y1a=rep(y1,length(y2))
+#	y2a=sort(rep(y2,length(y1)))
+#	ya=cbind(y1a,y2a)
+#	yaLDA=as.numeric(t(m1-m2)%*%Sp1%*%t(ya)-
+#					as.numeric(1/2*t(m1-m2)%*%Sp1%*%(m1+m2)))-log(p2/p1)
+#	
+#	boundLDA=abs(yaLDA)<0.05
+#	lines(lowess(y1a[boundLDA],y2a[boundLDA]),col=gray(0.6),lwd=1.5,lty=1)
+
 invisible(res)
 }
-
 
 
